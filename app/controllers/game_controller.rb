e31a4fbc55122fc33b9dae7e722bf7147cb51d9f
normoneil <normoneil@users.noreply.github.com>
2016-10-24 19:19:33 -0500

    Rock logic in place, now onto the others.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,11 +6,44 @@
 
 class GameController < ApplicationController
 
+  def game_turn(user_move, computer_move)
+    if user_move == computer_move
+      result = "tied"
+    elsif user_move == "paper" && computer_move == "rock"
+      result = "won"
+    elsif user_move == "paper" && computer_move == "scissors"
+      result = "lost"
+    elsif user_move == "scissors" && computer_move == "rock"
+      result = "lost"
+    elsif user_move == "scissors" && computer_move == "paper"
+      result = "won"
+    elsif user_move == "rock" && computer_move == "paper"
+      result = "lost"
+    elsif user_move == "rock" && computer_move == "scissors"
+      result = "won"
+    end
+      return result
+  end
+
   def user_plays_rock
+    @user_move = "rock"
+    @computer_move = ["rock", "paper", "scissors"].sample
+    @game_result = game_turn(@user_move, @computer_move)
+
     render("game/play_rock.html.erb")
+  end
+
+  def user_plays_paper
+    @play = rand(100)
+    render("game/play_paper.html.erb")
+  end
+
+  def user_plays_scissors
+    @play = rand(100)
+    render("game/play_scissors.html.erb")
   end
 
 end
 
-# no go to views, where all our HTML will live.  create a subfolder called
+# now go to views, where all our HTML will live.  create a subfolder called
 # game, then create the erb file in it.
--- config/routes.rb
+++ config/routes.rb
@@ -62,6 +62,8 @@ Rails.application.routes.draw do
   # This new file under app is the GameController class you just called
 
   get("/rock", { :controller => "game", :action => "user_plays_rock" })
+  get("/paper", { :controller => "game", :action => "user_plays_paper" })
+  get("/scissors", { :controller => "game", :action => "user_plays_scissors" })
 
 
 
